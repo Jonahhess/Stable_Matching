7e0e16247032085a07abc0d0e60da90501eef943
Jonah Hess <jonahhess@outlook.com>
2023-08-07 13:16:23 +0300

    refactored dating_pool

--- dating_pool.py
+++ dating_pool.py
@@ -1,4 +1,4 @@
-import random
+from random import shuffle
 from person import Person, Gender
 
 class Dating_Pool:
@@ -6,30 +6,29 @@ class Dating_Pool:
     women:list[Person]
 
     def __init__(self,n_men,n_women,loyalty) -> None:
-        self.men,self.women = self.create_men_and_women(n_men,n_women,loyalty)
+        self.create_men_and_women(n_men,n_women,loyalty)
         self.generate_preferences()
 
-    def create_men_and_women(self, n_men, n_women, loyalty):
-        men = []
-        women = []
+    def create_men_and_women(self, n_men, n_women, loyalty) -> None:
+        self.men = []
+        self.women = []
         for i in range(n_men):
             man = Person(i, Gender.MAN, loyalty)
-            men.append(man)
+            self.men.append(man)
         for i in range(n_women):
             woman = Person(i, Gender.WOMAN, loyalty)
-            women.append(woman)
-        return men, women
-
-    def generate_pref_list(self, person, people) -> None:
-        pref_list = [i for i in people]
-        random.shuffle(pref_list)
-        person.pref_list = pref_list
+            self.women.append(woman)
 
     def generate_preferences(self) -> None:
         for i in self.men:
             self.generate_pref_list(i, self.women)
         for i in self.women:
             self.generate_pref_list(i, self.men)
+
+    def generate_pref_list(self, person, people) -> None:
+        pref_list = [i for i in people]
+        shuffle(pref_list)
+        person.pref_list = pref_list
 
     def get_men(self):
         return self.men
